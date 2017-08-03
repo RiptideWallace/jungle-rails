cca396be0cf6f4a2038cb613d98bf9dc542336ef
RiptideWallace <brysonc1987@gmail.com>
2017-08-03 23:36:47 +0000

    Admin authorization added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"], except: :index
+
   def index
     @category = Category.order(id: :desc).all
   end
