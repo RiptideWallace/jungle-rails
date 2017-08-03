9a2fb64340da02a72b3b17dfcce61da85946f76d
RiptideWallace <brysonc1987@gmail.com>
2017-08-03 22:39:03 +0000

    Order ID added to email subject

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
     if order.valid?
       empty_cart!
       #Send email
-      ReceiptMailer.receipt_email(order, root_url).deliver_now
+      ReceiptMailer.receipt_email(order, root_url).deliver
       redirect_to order, notice: 'Your Order has been placed.'
     else
       redirect_to cart_path, error: order.errors.full_messages.first
